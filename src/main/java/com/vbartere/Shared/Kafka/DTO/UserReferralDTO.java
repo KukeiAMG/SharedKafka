60006ef716fc58d350c2d15ce5c8789338e4a071
KukeiAMG <159035810+KukeiAMG@users.noreply.github.com>
2025-05-05 13:59:02 +0500

    refactor(UserReferralDTO): remove name field and add invitedByCode to toString
    - Removed deprecated 'name' field from User model
    - Added 'invitedByCode' field to User model
    - Updated toString() method to include new field

--- src/main/java/com/vbartere/Shared/Kafka/DTO/UserReferralDTO.java
+++ src/main/java/com/vbartere/Shared/Kafka/DTO/UserReferralDTO.java
@@ -3,13 +3,10 @@ package com.vbartere.Shared.Kafka.DTO;
 public class UserReferralDTO {
 
     private Long id;
-    private String name;
-
     private String invitedByCode;
 
-    public UserReferralDTO(Long id, String name, String invitedByCode) {
+    public UserReferralDTO(Long id, String invitedByCode) {
         this.id = id;
-        this.name = name;
         this.invitedByCode = invitedByCode;
     }
 
@@ -29,19 +26,11 @@ public class UserReferralDTO {
             this.id = id;
         }
 
-    public String getName() {
-            return name;
-        }
-
-    public void setName(String name) {
-            this.name = name;
-        }
-
     @Override
     public String toString() {
         return "UserDTO{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", invitedByCode='" + invitedByCode + '\'' +
                 '}';
     }
 }
